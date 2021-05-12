7d5907679648eb6438b300ccab86b55fc22eb238
Andoner <Andsmith316@gmail.com>
2021-05-11 21:19:38 -0700

    game function works minus shuffle

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let toBeShuffledCards = [];
 // variable for cards selected
 let cardSelected = [];
 
+
 // set player enter name in button/prompt
 let enterName = document.querySelector("#myinput");
 // add eventListener for name button/prompt
@@ -77,6 +78,7 @@ function startGame() {
     card.addEventListener("click", (event) => {
       console.log(card);
       console.log(event.target.id);
+
     });
   });
 
@@ -84,23 +86,23 @@ function startGame() {
 let hiddenCards = document.querySelectorAll(".game-card");
 hiddenCards.forEach(function (hcard) {
     hcard.addEventListener("click", (event) => {
-    //   create time out function for when card is clicked to see vaule for 1 second before flipping back.
-      let delayMilli = 1500;
+    //   create time out function for when card is clicked to see vaule for 1 second before flipping back
+      let delayMilli = 1000;
       setTimeout(function(){
         event.target.childNodes[0].classList.toggle("hide");
-
       }, delayMilli);
 
       event.target.childNodes[0].classList.toggle("hide");
 
-      console.log(`this is: ${hcard}`);
+      console.log(`this is: ${hcard.firstChild}`);
       console.log(event.target.childNodes[0].id);
       if (cardSelected !== [0]) {
-        cardSelected.push(event.target.childNodes[0].id);
+        cardSelected.push(event.target);
       }
-      console.log(cardSelected);
+      console.log(cardSelected[0]);
+    //   if(cardSelected[1]){
       //  creat condition to check it cards are a match
-      if (cardSelected[0] == cardSelected[1]) {
+      if (cardSelected[0].dataset.dog == cardSelected[1].dataset.dog && cardSelected[0].firstChild.id == cardSelected[1].firstChild.id) {
         console.log("looped worked");
         // if match add a point to score
         score += 1;
@@ -109,51 +111,51 @@ hiddenCards.forEach(function (hcard) {
         // updates game-status tag
         
         // this works for one card to show its value after clicked
-            event.target.childNodes[0].classList.toggle("hide");
-                // for(let i =0; i< cardSelected.length; i++){
-                //     event.target.childNodes[0].classList.toggle("hide");
-                //     console.log(event.target);
-                //     // event.target.childNodes[0[1]].classList.toggle("hide");
-                // };
-        console.log(cardSelected[0]);
-        console.log(score);
+            cardSelected[1].firstChild.classList.toggle("hide");
+            cardSelected[0].firstChild.classList.toggle("hide");
+
 
         document.querySelector(".game-status").innerText = "Nice Job you found a match! Click any square below to continue";
-        
-        // end game if score equals 8
-        if (score == 8) {
-            document.querySelector(".game-status").innerText = "You found all the matches!";
-
-            alert("Congratulations you found all the matches!");
-            startGame();
-        };
-        // clear cardSelected Array?
+        console.log(cardSelected);
+        // clear cardSelected Array
         cardSelected = [];
+        console.log(cardSelected);
+        // end game if score equals 8
+        if (score == 2) {
+            document.querySelector(".game-status").innerText = "You found all the matches!";
+            let delayMilli2 = 5000;
+            setTimeout(() =>{
+                // alert("Congratulations you found all the matches!");
+                doAlert();
+                cardSelected = [];
+                location.reload();
+            }, delayMilli2);
+        };
         // if card is not the right match
-    } else if (cardSelected.length == 2 && cardSelected[0] !== cardSelected[1]) {         
+    } else if (cardSelected.length == 2 && cardSelected[0].firstChild.dataset.dog !== cardSelected[1].firstChild.dataset.dog) {         
             console.log("try again");
             document.querySelector(".game-status").innerText = "Sorry that wasnt a match try again!";
 
         //hide value of both cards selected
             cardSelected = [];
             //need compare that its not same square/location in array?
-        } else {
-            //cardSelected[0].classList.toggle("hide");
-            // cardSelected[1].classList.toggle("hide");
-            // event.target.childNodes[0].classList.toggle("hide");
-            // event.target.childNodes[1].classList.toggle("hide");
-            
-      }
+        } 
   // we will need to shuffle the cards
   // console.log(toBeShuffledCards);
     });
 });  
 
-
+let doAlert= () =>{
+    alert("Congratulations you found all the matches!");
+  
+};
+ let endGame = () =>{
+     startGameOver();
+ };
 
   // shuffle cards.
   console.log(toBeShuffledCards);
-        //     let shuffledCards = [];
+            // let shuffledCards = [];
         //     //   let shuffledCards = toBeShuffledCards[randomCard];
         // function shuffle(array){    
         //     for (let i = 0; i < toBeShuffledCards.length; i++) {
@@ -162,29 +164,39 @@ hiddenCards.forEach(function (hcard) {
         //         // if(randomCard != randomCard)
         //         shuffledCards.push(toBeShuffledCards[randomCard]);
         //     }}
-        //     shuffle(toBeShuffledCards);
-        //     // console.log(`this is: ${randomCard}`);
-        //     console.log(shuffledCards);
+//             let arrayShuffle = function(arr){
+//                 let newPos,
+//                     temp;
+                
 
-        //     // we need to hide the cards value and flip all cards at start
-        //     for (let i = 0; i < shuffledCards.length; i++) {
-        //         shuffledCards[i].classList.add("hide");
-        //     }
+//                 for( let i = arr.length -1; i > 0; i--){
+//                     newPos = Math.floor(Math. random() * (i +1));
+//                     temp = arr[i];
+//                     arr[i] = arr[newPos];
+//                     arr[newPos] = temp;
+//                 }
+//                 return arr;
+//             };
 
+//             let shuffledCards = arrayShuffle(toBeShuffledCards);
+//             console.log(shuffledCards);
 
+//         //     // we need to hide the cards value and flip all cards at start
+//             for (let i = 0; i < shuffledCards.length; i++) {
+//                 shuffledCards[i].classList.add("hide");
+//             }
+
+// console.log(shuffledCards);
       for(let i = 0;i< toBeShuffledCards.length; i++){
           toBeShuffledCards[i].classList.add("hide");
       }      
-  // console.log(toBeShuffledCards);
-  // end game
-  if (score == 8) {
-    prompt("Congratulations you found all the matches!");
-    startGame();
-  }
+  console.log(toBeShuffledCards);
+ 
 }
 
+
 // start game
-//  window.onload = startGame();
+ window.onload = startGame();
 
 
 
